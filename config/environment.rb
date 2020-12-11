c62bc621544a3423db98fb1575e68fd27ac75192
--add <andrewksmoker@gmail.com>
2020-12-11 18:52:19 -0500

    updating environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,21 @@
-ENV['SINATRA_ENV'] ||= "production"
-
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+Bundler.require
+#(:default, ENV['SINATRA_ENV'])
+require_all 'app'
 
-ActiveRecord::Base.establish_connection(
-  ENV['SINATRA_ENV'].to_sym
-)
+configure :development do 
+  ENV['SINATRA_ENV'] ||= "development"
+
+    ActiveRecord::Base.establish_connection(
+      ENV['SINATRA_ENV'].to_sym
+      )
+end 
+
+configure :production do 
+  ENV['SINATRA_ENV'] ||= "production"
+  ActiveRecord::Base.establish_connection(
+    ENV['SINATRA_ENV'].to_sym
+    )
+end 
 
 require './app/controllers/application_controller'
-require_all 'app'
